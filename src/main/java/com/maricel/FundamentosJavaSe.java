0136dc6f612c9bf4ce70960aac43e7b625e09146
Maricel1997 <maricelriveros19@gmail.com>
2023-03-15 20:25:47 -0300

    iniciar commit

---
+++ src/main/java/com/maricel/FundamentosJavaSe.java
@@ -0,0 +1,16 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.maricel;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FundamentosJavaSe {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
